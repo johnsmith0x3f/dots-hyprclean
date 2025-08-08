6021c07c5c7d534383b830f2f01595036891c422
John Smith <johnsmith0x3f@gmail.com>
2025-08-08 08:12:59 +0000

    feat: add step to link/unlink global config
    Prompt for user confirmation before every action.

--- hyprclean.sh
+++ hyprclean.sh
@@ -5,6 +5,10 @@ if ! type pacman &> /dev/null; then
 	exit 1
 fi
 
+cd "$( dirname "$0" )"
+
+
+
 declare -a deps=(
 	fastfetch
 	git
@@ -17,73 +21,66 @@ declare -a deps=(
 	wmenu
 )
 
-cd "$( dirname "$0" )"
-
 hyprclean-help() {
 	cat <<-EOF
 		Usage:
 		  $0 <OPTION>
 
 		Options:
-		  -i, --install     Install Hyprclean
-		  -u, --uninstall   Uninstall Hyprclean
+		  -i, --install     Install Hyprclean, requires sudo
+		  -u, --uninstall   Uninstall Hyprclean, requires sudo
 	EOF
 }
 
 hyprclean-install() {
-	echo -e "\x1B[1;32mInstalling packages...\x1B[0m"
-	sudo pacman -Sv --needed "${deps[@]}"
-	echo -e "\x1B[1;32mDone.\x1B[0m"
+	echo -en "\x1B[1;33mInstall dependencies (uses sudo)? [y/N]\x1B[0m "
+	read reply
+	if [[ "$reply" =~ ^[Yy](es)?$ ]]; then
+		sudo pacman -Sv --needed "${deps[@]}"
+	fi
 
 	echo ""
 
-	echo -e "\x1B[1;32mBacking up old configs...\x1B[0m"
-	for file in etc/*; do
-		if [[ -e "$XDG_CONFIG_HOME/${file##*/}" ]]; then
-			echo "$XDG_CONFIG_HOME/${file##*/}" "->" ".backup/etc/${file##*/}"
-		fi
-	done
-	for file in usr/share/*; do
-		if [[ -e "$XDG_DATA_HOME/${file##*/}" ]]; then
-			echo "$XDG_DATA_HOME/${file##*/}" "->" ".backup/usr/share/${file##*/}"
-		fi
-	done
-	echo -e "\x1B[1;32mDone. Old configs moved to $( dirname "$0" )/.backup\x1B[0m"
+	echo -en "\x1B[1;33mLink Hyprclean global configs (uses sudo)? [y/N]\x1B[0m "
+	read reply
+	if [[ "$reply" =~ ^[Yy](es)?$ ]]; then
+		sudo stow --verbose -d @ -t / .
+	fi
 
 	echo ""
 
-	echo -e "\x1B[1;32mLinking hyprclean configs...\x1B[0m"
-	stow --verbose -d etc -t "${XDG_CONFIG_HOME}" .
-	stow --verbose -d usr -t "${XDG_DATA_HOME%/*}" .
-	echo -e "\x1B[1;32mDone.\x1B[0m"
+	echo -en "\x1B[1;33mLink Hyprclean user configs? [y/N]\x1B[0m "
+	read reply
+	if [[ "$reply" =~ ^[Yy](es)?$ ]]; then
+		stow --verbose -d etc -t "${XDG_CONFIG_HOME}" .
+		stow --verbose -d usr -t "${XDG_DATA_HOME%/*}" .
+	fi
 }
 
 hyprclean-uninstall() {
-	echo -e "\x1B[1;32mUnlinking hyprclean configs...\x1B[0m"
-	stow --delete --verbose -d etc -t "${XDG_CONFIG_HOME}" .
-	stow --delete --verbose -d usr -t "${XDG_DATA_HOME%/*}" .
-	echo -e "\x1B[1;32mDone.\x1B[0m"
+	echo -en "\x1B[1;33mUninstall dependencies (uses sudo)? [y/N]\x1B[0m "
+	read reply
+	if [[ "$reply" =~ ^[Yy](es)?$ ]]; then
+		sudo pacman -Rnsv --needed "${deps[@]}"
+		echo -e "\x1B[1;32mDone.\x1B[0m"
+	fi
 
 	echo ""
 
-	echo -e "\x1B[1;32mRestoring backed up configs...\x1B[0m"
-	for file in etc/*; do
-		if [[ -e ".backup/etc/${file##*/}" ]]; then
-			echo ".backup/etc/${file##*/}" "->" "$XDG_CONFIG_HOME/${file##*/}"
-		fi
-	done
-	for file in usr/share/*; do
-		if [[ -e ".backup/usr/share/${file##*/}" ]]; then
-			echo ".backup/usr/share/${file##*/}" "->" "$XDG_DATA_HOME/${file##*/}"
-		fi
-	done
-	echo -e "\x1B[1;32mDone.\x1B[0m"
+	echo -en "\x1B[1;33mUnlink Hyprclean global configs (uses sudo)? [y/N]\x1B[0m "
+	read reply
+	if [[ "$reply" =~ ^[Yy](es)?$ ]]; then
+		sudo stow --delete --verbose -d @ -t / .
+	fi
 
 	echo ""
 
-	echo -e "\x1B[1;32mUninstalling packages...\x1B[0m"
-	sudo pacman -Rnsv "${deps[@]}"
-	echo -e "\x1B[1;32mDone.\x1B[0m"
+	echo -en "\x1B[1;33mUnlink Hyprclean user configs? [y/N]\x1B[0m "
+	read reply
+	if [[ "$reply" =~ ^[Yy](es)?$ ]]; then
+		stow --delete --verbose -d etc -t "${XDG_CONFIG_HOME}" .
+		stow --delete --verbose -d usr -t "${XDG_DATA_HOME%/*}" .
+	fi
 }
 
 case $1 in
